92048bb77ce7263a28b3160d0ec814e41e878bb8
Thamir <thamirsiddik@gmail.com>
2026-01-04 00:25:31 +0530

    chore: set up tailwind css and shadcn/ui

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { add } from '@orbit/math/add';
 
 @Controller()
 export class AppController {
@@ -8,7 +7,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    console.log("🚀 ~ AppController ~ getHello ~ add(1, 5):", add(1, 5))
     return this.appService.getHello();
   }
 }
