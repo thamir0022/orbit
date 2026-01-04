f8f9979d688b302a54439142bf9c6886944728ee
Thamir <thamirsiddik@gmail.com>
2026-01-04 19:44:26 +0530

    chore: add prettier config, prettier ignore, and nestjs eslint config

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: "es5",
+  printWidth: 100,
+  tabWidth: 2,
+  arrowParens: "avoid",
+  endOfLine: "lf",
+};
