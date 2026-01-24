1b90026ab6936aa0f056040256cb995dd56cfb92
Thamir <thamirpkd004@gmail.com>
2026-01-24 20:05:42 +0530

    chore(eslint): update eslint config

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -1,9 +1,9 @@
-import { nestJsConfig } from '@orbit/eslint-config/nest.js';
+import { nestJsConfig } from '@orbit/eslint-config/nest.js'
 
-/** @type {import("eslint").Linter.Config} */
+/** @type {import("eslint").Linter.Config[]} */
 export default [
   {
     ignores: ['eslint.config.mjs'],
   },
   ...nestJsConfig,
-];
+]
