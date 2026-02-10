ce4eca7c55650f82432c6aa827fce0e89ca918b6
Thamir <thamirpkd004@gmail.com>
2026-02-10 15:26:53 +0530

    refactor: move api and client folders into apps directory

--- apps/api/src/modules/auth/presentation/dtos/sign-up.response.dto.ts
+++ apps/api/src/modules/auth/presentation/dtos/sign-up.response.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { AuthenticatedUser } from './../../application/dto/sign-in.result'
+import { AuthenticatedUser } from '../../application/dto/sign-in.result'
 
 /**
  * Sign Up Response DTO
