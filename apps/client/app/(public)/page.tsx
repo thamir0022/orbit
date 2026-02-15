49501fe61146c7d782e106a86ebe4fb90d857c69
Thamir <thamirpkd004@gmail.com>
2026-02-16 01:19:49 +0530

    chore: use user from store

--- apps/client/app/(public)/page.tsx
+++ apps/client/app/(public)/page.tsx
@@ -1,6 +1,14 @@
-import { Button } from '@/components/ui/button'
+'use client'
+
+import { useUserStore } from '@/entities/user/model/user.store'
+import { Button } from '@/shared/ui/button'
 
 export default function Page() {
+  const { user, accessToken } = useUserStore()
+
+  console.log('USER', user)
+  console.log('TOKEN', accessToken)
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
