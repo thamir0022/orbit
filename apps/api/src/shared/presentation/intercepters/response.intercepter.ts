c4cf8ce86f7b33877d434b4069a76b0df6894ad8
Thamir <thamirpkd004@gmail.com>
2026-02-16 13:18:40 +0530

    feat(core): implement response message decorator for dynamic success messages

--- apps/api/src/shared/presentation/intercepters/response.intercepter.ts
+++ apps/api/src/shared/presentation/intercepters/response.intercepter.ts
@@ -8,12 +8,15 @@ import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
 import { Request, Response } from 'express'
 import { ApiResponseDto } from '@/shared/presentation/dtos/api-response.dto'
+import { Reflector } from '@nestjs/core'
+import { RESPONSE_MESSAGE_KEY } from '../decorators/response-message.decorator'
 
 @Injectable()
 export class ResponseInterceptor<T> implements NestInterceptor<
   T,
   ApiResponseDto<T>
 > {
+  constructor(private reflector: Reflector) {}
   intercept(
     context: ExecutionContext,
     next: CallHandler
@@ -23,12 +26,19 @@ export class ResponseInterceptor<T> implements NestInterceptor<
     const response = ctx.getResponse<Response>()
     const statusCode = response.statusCode
 
+    // Retrieve the message from the handler or class
+    const message =
+      this.reflector.getAllAndOverride<string>(RESPONSE_MESSAGE_KEY, [
+        context.getHandler(),
+        context.getClass(),
+      ]) || 'Request processed successfully'
+
     return next.handle().pipe(
       map((data: T) => {
         return {
           success: true,
           statusCode,
-          message: 'Success',
+          message,
           data,
           error: null,
           timestamp: new Date().toISOString(),
