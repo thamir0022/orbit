c53e11583dcf661c5ad1b558c17ec7c1b7471ba5
Thamir <thamirpkd004@gmail.com>
2026-09-12 21:04:49 +0530

    chore(redis): update redis module for latest implementation

--- apps/api/src/shared/infrastructure/cache/redis.module.ts
+++ apps/api/src/shared/infrastructure/cache/redis.module.ts
@@ -1,8 +1,7 @@
-import { Module, Global } from '@nestjs/common'
+import { Global, Module } from '@nestjs/common'
 import { CacheModule } from '@nestjs/cache-manager'
-import KeyvRedis from '@keyv/redis'
-import { Keyv } from 'keyv'
-import { CacheableMemory } from 'cacheable'
+import { createKeyv, Keyv } from '@keyv/redis'
+import { KeyvCacheableMemory } from 'cacheable'
 import {
   type IRedisConfig,
   REDIS_CONFIG,
@@ -17,9 +16,9 @@ import {
         return {
           stores: [
             new Keyv({
-              store: new CacheableMemory({ ttl: 60000, lruSize: 5000 }),
+              store: new KeyvCacheableMemory({ ttl: 60000, lruSize: 5000 }),
             }),
-            new KeyvRedis(config.redisUri),
+            createKeyv(config.redisUri),
           ],
         }
       },
