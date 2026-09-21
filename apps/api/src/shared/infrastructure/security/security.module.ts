32ba1bcc7e7342a4c9b647c413066fc99caea053
Thamir <thamirpkd004@gmail.com>
2026-09-21 16:02:40 +0530

    refactor(auth): remove jwt access and refresh token guards

--- apps/api/src/shared/infrastructure/security/security.module.ts
+++ apps/api/src/shared/infrastructure/security/security.module.ts
@@ -2,8 +2,6 @@ import { Global, Module } from '@nestjs/common'
 import { JwtModule } from '@nestjs/jwt'
 import { BcryptPasswordHasher } from './hashing/bcrypt-password-hasher.service'
 import { JwtTokenGenerator } from './tokens/jwt-token-generator.service'
-import { RefreshTokenGuard } from './guards/refresh-token.guard'
-import { AccessTokenGuard } from './guards/access-token.guard'
 import { PASSWORD_HASHER } from '../../application/ports/password-hasher.interface'
 import { TOKEN_GENERATOR } from '../../application/ports/token-generator.interface'
 
@@ -19,14 +17,7 @@ import { TOKEN_GENERATOR } from '../../application/ports/token-generator.interfa
       provide: TOKEN_GENERATOR,
       useClass: JwtTokenGenerator,
     },
-    RefreshTokenGuard,
-    AccessTokenGuard,
   ],
-  exports: [
-    PASSWORD_HASHER,
-    TOKEN_GENERATOR,
-    RefreshTokenGuard,
-    AccessTokenGuard,
-  ],
+  exports: [PASSWORD_HASHER, TOKEN_GENERATOR],
 })
 export class SecurityModule {}
