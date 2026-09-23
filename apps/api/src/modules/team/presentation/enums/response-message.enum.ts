5872091712981d954f90678dbd4acd08ccc4b5a7
Thamir <thamirpkd004@gmail.com>
2026-09-23 19:11:12 +0530

    feat(team): add team response message enum

---
+++ apps/api/src/modules/team/presentation/enums/response-message.enum.ts
@@ -0,0 +1,7 @@
+export enum TeamResponseMessage {
+  TEAM_CREATED = 'Team created successfully.',
+  TEAMS_FETCHED = 'Teams retrieved successfully.',
+  TEAM_FETCHED = 'Team retrieved successfully.',
+  TEAM_UPDATED = 'Team updated successfully.',
+  TEAM_DELETED = 'Team deleted successfully.',
+}
